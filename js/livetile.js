38b5ac78c183970cbf8ef283f075f19683d7eebb
mhawker <michael.hawker@outlook.com>
2017-07-19 14:43:12 -0700

    Add Secondary Tile Activation Support

---
+++ js/livetile.js
@@ -0,0 +1,59 @@
+﻿
+var logoSecondaryTileId = "JSToastRouting.Logo";
+
+function addLiveTile(title, parameters) {
+    if (typeof Windows != 'undefined') {
+        // Prepare package images for all four tile sizes in our tile to be pinned as well as for the square30x30 logo used in the Apps view.  
+        var square150x150Logo = new Windows.Foundation.Uri("ms-appx:///Assets/square150x150Tile-sdk.png");
+        var wide310x150Logo = new Windows.Foundation.Uri("ms-appx:///Assets/wide310x150Tile-sdk.png");
+        var square310x310Logo = new Windows.Foundation.Uri("ms-appx:///Assets/square310x310Tile-sdk.png");
+        var square30x30Logo = new Windows.Foundation.Uri("ms-appx:///Assets/square30x30Tile-sdk.png");
+
+        // During creation of secondary tile, an application may set additional arguments on the tile that will be passed in during activation.
+        // These arguments should be meaningful to the application. In this sample, we'll pass in the date and time the secondary tile was pinned.
+        var tileActivationArguments = JSON.stringify(parameters);  //logoSecondaryTileId + " WasPinnedAt=" + new Date().toLocaleTimeString();
+
+        // Create a Secondary tile with all the required arguments.
+        // Note the last argument specifies what size the Secondary tile should show up as by default in the Pin to start fly out.
+        // It can be set to TileSize.Square150x150, TileSize.Wide310x150, or TileSize.Default.  
+        // If set to TileSize.Wide310x150, then the asset for the wide size must be supplied as well.
+        // TileSize.Default will default to the wide size if a wide size is provided, and to the medium size otherwise. 
+        var secondaryTile = new Windows.UI.StartScreen.SecondaryTile(logoSecondaryTileId,
+            title,
+            tileActivationArguments,
+            square150x150Logo,
+            Windows.UI.StartScreen.TileSize.square150x150);
+
+        if (!(Windows.Foundation.Metadata.ApiInformation.isTypePresent(("Windows.Phone.UI.Input.HardwareButtons")))) {
+            secondaryTile.visualElements.wide310x150Logo = wide310x150Logo;
+            secondaryTile.visualElements.square310x310Logo = square310x310Logo;
+        }
+
+        // Like the background color, the square30x30 logo is inherited from the parent application tile by default. 
+        // Let's override it, just to see how that's done.
+        secondaryTile.visualElements.square30x30Logo = square30x30Logo;
+
+        // The display of the secondary tile name can be controlled for each tile size.
+        // The default is false.
+        secondaryTile.visualElements.showNameOnSquare150x150Logo = true;
+
+        if (!(Windows.Foundation.Metadata.ApiInformation.isTypePresent(("Windows.Phone.UI.Input.HardwareButtons")))) {
+            secondaryTile.visualElements.showNameOnWide310x150Logo = true;
+            secondaryTile.visualElements.showNameOnSquare310x310Logo = true;
+        }
+
+        // Specify a foreground text value.
+        // The tile background color is inherited from the parent unless a separate value is specified.
+        secondaryTile.visualElements.foregroundText = Windows.UI.StartScreen.ForegroundText.Dark;
+
+        // Set this to false if roaming doesn't make sense for the secondary tile.
+        // The default is true;
+        secondaryTile.roamingEnabled = false;
+
+        // OK, the tile is created and we can now attempt to pin the tile.
+        // Since pinning a secondary tile on Windows Phone will exit the app and take you to the start screen, any code after 
+        // RequestCreateForSelectionAsync or RequestCreateAsync is not guaranteed to run.  For an example of how to use the OnSuspending event to do
+        // work after RequestCreateForSelectionAsync or RequestCreateAsync returns, see Scenario9_PinTileAndUpdateOnSuspend in the SecondaryTiles.WindowsPhone project.
+        secondaryTile.requestCreateAsync();
+    }
+}
--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 ﻿// Hook into Buttons
 var arguments = ""
 window.addEventListener("DOMContentLoaded", (ev) => {
+    // Toasts
     document.getElementById("toast1").addEventListener("click", () => {
         toastNotification("Making Toast!", { "url": "toast1", "message": "toast", "user": "67890" });
     });
@@ -8,6 +9,12 @@ window.addEventListener("DOMContentLoaded", (ev) => {
         toastNotification("Making Burnt Toast!", { "url": "toast2", "message": "burnt", "user": "12345" });
     });
 
+    // Live Tiles
+    document.getElementById("liveadd").addEventListener("click", () => {
+        addLiveTile('Test Tile', { time: new Date().toLocaleTimeString(), message: "hello from tile", "user": "12345" });
+    });
+
+    // Show activation arguments
     document.getElementById("arguments").innerText = arguments;
 });
 
@@ -41,46 +48,3 @@ if (window.Windows) {
         }
     });
 }
-
-// Pop-up General Toast Notification to Desktop/Action Center
-function toastNotification(message, parameters) {
-    // Log the message to the console
-    console.log("Notification: " + message);
-
-    if (typeof Windows != 'undefined') {
-        //Error detection
-        // var text = document.createTextNode("Calling the Notifications")
-        // document.body.appendChild(text);
-        // Log to the console
-        var Notifications = Windows.UI.Notifications;
-        //Get the XML template where the notification content will be suplied
-        var template = Notifications.ToastTemplateType.toastImageAndText01;
-        var toastXml = Notifications.ToastNotificationManager.getTemplateContent(template);
-        //Supply the text to the XML content
-        var toastTextElements = toastXml.getElementsByTagName("text");
-        toastTextElements[0].appendChild(toastXml.createTextNode(message));
-        //Supply an image for the notification
-        var toastImageElements = toastXml.getElementsByTagName("image");
-        //Set the image this could be the background of the note, get the image from the web
-        toastImageElements[0].setAttribute("src", "http://lorempixel.com/44/36/food/");
-        toastImageElements[0].setAttribute("alt", "red graphic");
-        //Specify a long duration
-        var toastNode = toastXml.selectSingleNode("/toast");
-        toastNode.setAttribute("duration", "long");
-        //Specify the audio for the toast notification
-        var toastNode = toastXml.selectSingleNode("/toast");
-        var audio = toastXml.createElement("audio");
-        audio.setAttribute("src", "ms-winsoundevent:Notification.IM");
-        //Specify launch paramater
-        toastXml.selectSingleNode("/toast").setAttribute("launch", JSON.stringify(parameters)); // Note: Would set these parameters to capture when relaunching the application
-        //Create a toast notification based on the specified XML
-        var toast = new Notifications.ToastNotification(toastXml);
-        //Send the toast notification
-        var toastNotifier = Notifications.ToastNotificationManager.createToastNotifier();
-        toastNotifier.show(toast);
-
-    } else {
-        //TODO: Fallback to website functionality
-        console.log("ERROR: No Windows namespace was detected");
-    }
-};
